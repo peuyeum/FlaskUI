46d98d2eddda9c47cbe44077665f6cc2f76cca22
Rolly Maulana Awangga <rolly@awang.ga>
2018-08-15 11:03:23 +0700

    done

--- main.py
+++ main.py
@@ -3,56 +3,27 @@ from flask import render_template
 from flask import Flask,abort,Response
 app = Flask(__name__)
 
-PRODUCTS = {
-    'iphone': {
-        'name': 'iPhone 5S',
-        'category': 'Phones',
-        'price': 699,
-    },
-    'galaxy': {
-        'name': 'Samsung Galaxy 5',
-        'category': 'Phones',
-        'price': 649,
-    },
-    'ipad-air': {
-        'name': 'iPad Air',
-        'category': 'Tablets',
-        'price': 649,
-    },
-    'ipad-mini': {
-        'name': 'iPad Mini',
-        'category': 'Tablets',
-        'price': 549
-    }
-}
 
 @app.route("/")
 def home():
 	name="ganteng"
-	return render_template('home.html', products=PRODUCTS, SignIn=True, SignUp=True)
+	return render_template('home.peuyeum', SignIn=True, SignUp=True)
 	
 @app.route("/process")
 def process():
 	name="ganteng"
-	return render_template('process.html', products=PRODUCTS, SignIn=True, SignUp=True)
+	return render_template('process.peuyeum', SignIn=True, SignUp=True)
 
-
-@app.route('/product/<key>')
-def product(key):
-    product = PRODUCTS.get(key)
-    if not product:
-        abort(404)
-    return render_template('product.html', product=product)
-
-@app.route("/login")
-def login():
+@app.route("/signin")
+def signin():
 	name="ganteng"
-	return render_template('login.html', name=name)
+	return render_template('signin.peuyeum', SignIn=True, SignUp=True)
 
 @app.route("/signup")
 def signup():
 	name="ganteng"
-	return render_template('signup.html', name=name)
+	return render_template('signup.peuyeum', SignIn=True, SignUp=True)
+
 	
 
 
